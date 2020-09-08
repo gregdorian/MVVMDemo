487b246be958d491e18dbfc41fe839e2ffb8b205
german valencia <gerger2024@hotmail.com>
2020-09-08 18:28:22 -0500

    MVM Base como debe ser

--- MVVMDemo/App.xaml.cs
+++ MVVMDemo/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVVMDemo.Views;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,10 @@ namespace MVVMDemo
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            UserRegistrationView usrVista = new UserRegistrationView();
+            usrVista.Show();
+        }
     }
 }
---
+++ MVVMDemo/ViewModels/StudentViewModel.cs
@@ -0,0 +1,68 @@
+﻿using MVVMDemo.Model;
+using MVVMDemo.Utilities;
+using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace MVVMDemo.ViewModels
+{
+    public class StudentViewModel : ViewModelBase
+    {
+        private Student _student;
+        private ObservableCollection<Student> _students;
+        private ICommand _SubmitCommand;
+        public Student Student
+        {
+            get
+            {
+                return _student;
+            }
+            set
+            {
+                _student = value;
+                NotifyPropertyChanged("Student");
+            }
+        }
+        public ObservableCollection<Student> Students
+        {
+            get
+            {
+                return _students;
+            }
+            set
+            {
+                _students = value;
+                NotifyPropertyChanged("Students");
+            }
+        }
+        public ICommand SubmitCommand
+        {
+            get
+            {
+                if (_SubmitCommand == null)
+                {
+                    _SubmitCommand = new RelayCommand(param => this.Submit(),
+                        null);
+                }
+                return _SubmitCommand;
+            }
+        }
+        public StudentViewModel()
+        {
+            Student = new Student();
+            Students = new ObservableCollection<Student>();
+            Students.CollectionChanged += new System.Collections.Specialized.NotifyCollectionChangedEventHandler(Students_CollectionChanged);
+        }
+        //Whenever new item is added to the collection, am explicitly calling notify property changed  
+        void Students_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            NotifyPropertyChanged("Students");
+        }
+        private void Submit()
+        {
+            Student.JoiningDate = DateTime.Today.Date;
+            Students.Add(Student);
+            Student = new Student();
+        }
+    }
+}
